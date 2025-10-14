d09865b621b2d2317fd5ecaa191efd4f220c23c1
ConnorFriedman10 <73201019+ConnorFriedman10@users.noreply.github.com>
2025-10-13 21:41:52 -0400

    small changes with supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { SearchBar } from './components/SearchBar'
+import { supabase } from './supabase'
 
 function App() {
-<<<<<<< HEAD
-  // const [count, setCount] = useState(0)
-  // const name = "Name"
-=======
-  const [count, setCount] = useState(0)
-  const name = "User Name"
->>>>>>> 38e9dd2effe2c131271218eb0bbb4087dbebf2d0
+  const [uni_name, setName] = useState(""); // Initialized as an empty string.
+  const [universities, setUni] = useState([]); // Initialized as an empty array.
 
   return (
     <div className="App">
@@ -23,4 +19,17 @@ function App() {
   )
 }
 
+async function fetchUniversities() {
+    try {
+      const { data, error } = await supabase // Destructure the Supabase call
+            .from("uni_names") // From the "Groceries" table
+            .select("*"); // Select (fetch) everything
+      if (error) throw error; // If there is an error, throw it
+      if (data != null) { // If there is data fetched
+        setUni(data); // Set our groceries state variable to the data
+      }
+    } catch (error) {
+      alert(error); // If an error is caught, alert it on the client
+    }
+  }
 export default App
