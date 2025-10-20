e6ff743307efdcba3976c53f6ca132bfd485fa8e
Connor Friedman <friedman.connor99@gmail.com>
2025-10-20 19:15:08 -0400

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
 import './App.css'
 import { SearchBar } from './components/SearchBar'
-import { supabase } from './supabase'
+import { SearchResultsList } from './components/SearchResultsList'
 
 function App() {
-  const [uni_name, setName] = useState(""); // Initialized as an empty string.
-  const [universities, setUni] = useState([]); // Initialized as an empty array.
-
-  async function fetchUniversities() {
-      try {
-        const { data, error } = await supabase // Destructure the Supabase call
-              .from("uni_names") // From the "Groceries" table
-              .select("*"); // Select (fetch) everything
-        if (error) throw error; // If there is an error, throw it
-        if (data != null) { // If there is data fetched
-          setUni(data); // Set our groceries state variable to the data
-        }
-      } catch (error) {
-        alert(error); // If an error is caught, alert it on the client
-      }
-    }
+  const [results, setResults] = useState([])
 
   return (
     <div className="App">
       <div className="search-bar-container">
-        <SearchBar />
-        <div>Search Results</div>
+        <SearchBar setResults={setResults}/>
+        <SearchResultsList results={results}/>
       </div>
     </div>
   )
