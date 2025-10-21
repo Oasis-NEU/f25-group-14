1c23c5eec69b108def126778fcaf012fb7750a23
Connor Friedman <friedman.connor99@gmail.com>
2025-10-21 14:46:20 -0400

    to do (10/21)

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </BrowserRouter>
 )
+
+//to do:
+//add an embedded table into the app-
+//  overtable (contains info on universities and reference to sub table) -> subtable (uni club info)
