a0ac69f195baa8d28c9baf1309df29d827ae9dad
Connor Friedman <friedman.connor99@gmail.com>
2025-10-21 14:06:32 -0400

    added router capabilities

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import { SearchBar } from './components/SearchBar'
 import { SearchResultsList } from './components/SearchResultsList'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
   const [results, setResults] = useState([])
 
   return (
+    <BrowserRouter>
     <div className="App">
       <div className="search-bar-container">
         <SearchBar setResults={setResults}/>
         <SearchResultsList results={results}/>
       </div>
     </div>
+    </BrowserRouter>
   )
 }
 
