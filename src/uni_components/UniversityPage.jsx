7bf2ddd7fb88d1fbcd1e994d18df87a8eedc2a65
Judah29 <Judahpotato@gmail.com>
2025-11-11 19:11:01 -0500

    save 2nd page

--- src/uni_components/UniversityPage.jsx
+++ src/uni_components/UniversityPage.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 import { supabase } from '../supabase'
 import { UniSearchBar } from './UniSearchBar'
 
+import NEULogo from '/src/uni_components/Imgs/NEU_Logo.png'
+
 export const UniversityPage = () => {
   const { id } = useParams()
   const [university, setUniversity] = useState(null)
