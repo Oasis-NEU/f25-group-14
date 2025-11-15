05ea76b01fea6a775924960a25ba76fe3217fd91
MiloB07 <bell.mil@northeastern.edu>
2025-11-15 12:26:56 -0500

    expand beta 1
    drfgoierfjerfoijh

--- src/uni_components/ClubGrid.jsx
+++ src/uni_components/ClubGrid.jsx
@@ -4,11 +4,13 @@ import './ClubGrid.css';
 export const ClubGrid = ({ result }) => {
   const [expanded, setExpanded] = useState(false);
   return (
-    <div 
-          className="club-card">
-          className={`card-card ${expanded ? 'expanded' : ''}`}
-          onClick={() => setExpanded(!expanded)}
+    <div
+        className={`club-card ${expanded ? 'expanded' : ''}`}
+        onClick={() => setExpanded(!expanded)}
+    >
       <div className="club-img">🦝</div>
+
+      
       <div className="club-info">
         <h2>{result.club}</h2>
         <p>{result.description}</p>
