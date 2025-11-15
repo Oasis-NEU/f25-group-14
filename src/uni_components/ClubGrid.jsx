189f109d2cdb9bfe6a2db3f8e855df35d29a24d5
MiloB07 <bell.mil@northeastern.edu>
2025-11-15 11:36:59 -0500

    on click club grid
    w9eufher98h

--- src/uni_components/ClubGrid.jsx
+++ src/uni_components/ClubGrid.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import './ClubGrid.css';
 
 export const ClubGrid = ({ result }) => {
+  const [active, setActive] = useState(false);
   return (
-    <div className="club-card">
+    <div 
+          className="club-card">
+          className={`card-card ${active ? 'active' : ''}`}
+          onClick={() => setActive(!active)}
       <div className="club-img">🦝</div>
       <div className="club-info">
         <h2>{result.club}</h2>
