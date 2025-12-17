ded03e7c99d95591e8815e6feb9c9cb6b849a1bb
Ryan Marshall <ryawmar@gmail.com>
2025-12-17 00:25:14 -0500

    changed data collection

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 """
 Current list of items to resolve
+* General Scraping Formatting:
+    - To make it so things are more organized, and to avoid whitespace issues and preserve a pages natural flow like it does in campus labs, I will individually 
+      search for the p tags in the usersupplied div and take the contents of each on, labelling them with dictionaries so the fellas know what to do.
 * Whitespace:
     - Wait until later once the description data is suffiecntly cleaned to avoid possible errors.
 * Getting images:
@@ -12,6 +15,7 @@ from dataclasses import dataclass
 from typing import Any, Optional
 import time, json
 from selenium import webdriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -34,6 +38,24 @@ def get_contact_info(driver: webdriver) -> list[Optional[str]]:
             return_list[2] = elements[i].get_attribute("href")
     return return_list
 
+#accepts a list of webelement objects, analyzes what type of object it is, and then takes the text and assigns it as a corresponding key
+#I'm pretty sure the only elements that are in this div are p elements and ul -> 
+#returns a list of either p and ul elements and their texts as tuples. If there is a ul element, it will loop through it and take all of the li elements and put them
+# in a list which will then be assigned to 'ul' in a tuple.   
+def scan_user_div(driver: webdriver) -> list[(str, Any)]:
+    return_list: list[(str, str)] = []
+    root_div = waiting_time.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.bodyText-large.userSupplied")))
+    elements: list[WebElement] = root_div.find_elements(By.CSS_SELECTOR, "p, ul")
+    for element in elements:
+        if element.tag_name == 'p':
+            return_list.append(('p', element.text.replace('\n\n', " ").replace('\n', " ")))
+        elif element.tag_name == 'ul':
+            ul_list: list[str] = [ul_element.text.replace('\n\n', " ").replace('\n', " ") for ul_element in element.find_elements(By.TAG_NAME, 'li')]
+            return_list.append(('ul', ul_list))
+        else:
+            print("Unidentified element in div")
+    return return_list
+
 opts = Options()
 opts.add_argument("--disable-dev-shm-usage")
 opts.add_argument("--disable-extensions")
@@ -47,7 +69,7 @@ opts.add_argument("--no-sandbox")
 @dataclass
 class Club():
     title: str
-    description: str
+    description: list[str, Any]
     logo_url: Optional[str]
     email: Optional[str]
     instagram: Optional[str]
@@ -59,12 +81,13 @@ club_list: list[Club] = []
 driver = webdriver.Chrome(options=opts)
 waiting_time = WebDriverWait(driver, 10)
 
-driver.get("https://neu.campuslabs.com/engage/organizations")
+driver.get("https://wit.campuslabs.com/engage/organizations")
 print(driver.title)
 button_element = driver.find_element(By.XPATH, "//div[@class='outlinedButton']//button[contains(., 'Load More')]")
 print(button_element.text)
 
 #Loads all of the data before scraping so all the listings can be clicked
+
 while True:
     try:
         #Click the button after waiting for it to load
@@ -99,7 +122,9 @@ for i in range(len(href_list)):
     #wait until the h1 element has loaded before grabbing. This ensures that the rest of the page likely has loaded too
     club_name = waiting_time.until(EC.presence_of_element_located((By.XPATH, "//h1"))).text
     #grabs the description of the club by getting all of the p elements and then combining them into one
-    description = waiting_time.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.bodyText-large.userSupplied"))).text
+    
+    description: list[str, Any] = scan_user_div(driver)
+
     #attempt to grab image_url
     try:
         logo_url = driver.find_element(By.XPATH, "//img").get_attribute("src")
@@ -114,15 +139,11 @@ for i in range(len(href_list)):
 
     links: Optional[str] = get_contact_info(driver)
 
-    #replace every \n with a space
-    description = description.replace('\n\n', " ").replace('\n', " ")
-
     #create new club object and append to list
     club_list.append(Club(club_name, description, logo_url, email, links[0], links[1], links[2]))
 
 #stop driver
 driver.quit()
-
 
 #test to see if the data is good
 print(club_list)
@@ -131,5 +152,5 @@ club_data: list[list[Optional[str]]] = [[club.title, club.description, club.logo
                                          club.instagram, club.facebook, club.external_link] for club in club_list]
 
 #write into a json file
-with open('northeastern_data.json', 'w') as f:
+with open('wentworth_data.json', 'w') as f:
     json.dump(club_data, f, ensure_ascii=False)
