eba78f3dec1d2e42363d22bbc5e4f0551459f6fb
Connor Friedman <friedman.connor99@gmail.com>
2025-12-29 15:19:38 -0500

    small lil update

--- src/review_components/ReviewPage.jsx
+++ src/review_components/ReviewPage.jsx
@@ -1,10 +1,10 @@
 export default function ReviewPage() {
 
     
-
+    
     return (
         <div>
-            <p>hi</p>
+            <p>this is the reviwe page, write review on the top and see others on the bottom</p>
         </div>
     )
 }
