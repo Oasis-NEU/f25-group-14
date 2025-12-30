0c3e1470f4f15d3fa53bf878953cbf8c874be3a3
ConnorFriedman10 <friedman.connor99@gmail.com>
2025-12-29 22:47:40 -0500

    fetch reviews

--- src/review_components/ReviewPage.jsx
+++ src/review_components/ReviewPage.jsx
@@ -1,7 +1,26 @@
-export default function ReviewPage() {
+import { supabase } from '../supabase';
+import React, { useState, useEffect } from 'react';
 
+export default function ReviewPage({}) {
+    const { id } = useParams()
+    const [reviews, set_reviews] = useState([])
     
+    useEffect(() => {
+        async function fetch_reviews() {
+            const {data, error} = await supabase
+                .from('reviews')
+                .select('*')
+
+            if (error) {
+                console.error('Error fetching reviews:', error);
+                return;
+            }
+            set_reviews(data);
+        }
+        fetch_reviews();
+    }, [id])
     
+
     return (
         <div>
             <p>this is the reviwe page, write review on the top and see others on the bottom</p>
